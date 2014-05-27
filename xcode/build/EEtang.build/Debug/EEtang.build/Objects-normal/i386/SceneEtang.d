7e3357244e50ac05628aa7079764b32fe9d34397
yiwenl <bongiovi015@gmail.com>
2014-05-27 23:13:52 +0100

    ignore
    Signed-off-by: yiwenl <bongiovi015@gmail.com>

--- xcode/build/EEtang.build/Debug/EEtang.build/Objects-normal/i386/SceneEtang.d
+++
@@ -1,8 +0,0 @@
-dependencies: /Users/yiwenl/Documents/cinder/Etang/src/SceneEtang.cpp \
-  /Users/yiwenl/Documents/cinder/Etang/src/SceneEtang.h \
-  ../../../../Development/Cinder/include/cinder/gl/Texture.h \
-  ../../../../Development/Cinder/include/cinder/gl/Fbo.h \
-  /Users/yiwenl/Documents/cinder/Etang/xcode/../src/bongiovi/Scene.h \
-  ../../../../Development/Cinder/include/cinder/Camera.h \
-  /Users/yiwenl/Documents/cinder/Etang/xcode/../src/bongiovi/SceneQuat.h \
-  /Users/yiwenl/Documents/cinder/Etang/xcode/../src/bongiovi/Utils.h
