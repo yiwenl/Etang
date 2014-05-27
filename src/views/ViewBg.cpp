577ff527c1183bee5a236a8a2f4a7a08b518a8da
yiwenl <bongiovi015@gmail.com>
2014-05-27 23:12:14 +0100

    update ignore files
    Signed-off-by: yiwenl <bongiovi015@gmail.com>

---
+++ src/views/ViewBg.cpp
@@ -0,0 +1,9 @@
+//
+//  ViewBg.cpp
+//  EEtang
+//
+//  Created by Yiwen on 27/05/2014.
+//
+//
+
+#include "ViewBg.h"
---
+++ src/views/ViewBg.h
@@ -0,0 +1,14 @@
+//
+//  ViewBg.h
+//  EEtang
+//
+//  Created by Yiwen on 27/05/2014.
+//
+//
+
+#ifndef __EEtang__ViewBg__
+#define __EEtang__ViewBg__
+
+#include <iostream>
+
+#endif /* defined(__EEtang__ViewBg__) */
